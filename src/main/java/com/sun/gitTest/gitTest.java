ed9f95ba72479939c94c83f9b65334826a35095d
sunzhongwei <1091871415@qq.com>
2022-10-08 15:52:08 +0800

    第二次 commit

--- src/main/java/com/sun/gitTest/gitTest.java
+++ src/main/java/com/sun/gitTest/gitTest.java
@@ -3,5 +3,6 @@ package com.sun.gitTest;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("测试git");
+        System.out.println("测试版本git2");
     }
 }
